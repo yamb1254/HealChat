40cb1e2c3e89ef249f8621017cb57f75c2c61ab3
Yam Biton <yamb1254@gmail.com>
2024-02-28 17:50:26 +0200

    docker + strat vite project

---
+++ HealChat/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 3000,
+  strictPort: true,
+ },
+ server: {
+  port: 3000,
+  strictPort: true,
+  host: true,
+  hmr: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+ },
+});
