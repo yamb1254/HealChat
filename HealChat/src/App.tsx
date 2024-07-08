af6a94eb60c8a8b6c36d05305188e60d70bf576e
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2024-07-08 14:45:07 +0300

    fixing stuiut

--- HealChat/src/App.tsx
+++ HealChat/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
--- HealChat/src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { isAdminAuthenticated } from "../utils/auth";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  return isAdminAuthenticated() ? children : <Navigate to="/admin/login" />;
-};
-
-export default ProtectedRoute;
