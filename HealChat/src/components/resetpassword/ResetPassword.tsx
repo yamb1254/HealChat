f59486a273fe8017dc4f49ebe6f7abc1cbe55ef3
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2024-07-13 12:13:18 +0300

    Update ResetPassword.tsx

--- HealChat/src/components/resetpassword/ResetPassword.tsx
+++ HealChat/src/components/resetpassword/ResetPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
