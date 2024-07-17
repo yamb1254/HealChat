1e2bfa1501374027871560c49b82e3096148cb06
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-18 01:00:56 +0300

    Finisih the chat

--- HealChat/src/components/resetpassword/ResetPassword.tsx
+++ HealChat/src/components/resetpassword/ResetPassword.tsx
@@ -18,7 +18,7 @@ const ResetPassword = () => {
     }
 
     try {
-      await axios.post("/api/auth/reset-password", {
+      await axios.post("http://localhost:5000/api/auth/reset-password", {
         token,
         newPassword,
       });
