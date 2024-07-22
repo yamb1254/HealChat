9c8486abcff300dd5e19e329a08b2acc36521227
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-22 12:38:56 +0300

    asf

--- HealChat/src/api/client.tsx
+++ HealChat/src/api/client.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://healchat.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
