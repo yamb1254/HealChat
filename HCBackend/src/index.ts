7d2ef8cb933336ed4bf1c165e63b2a9f887533c2
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-08-05 14:31:27 +0300

    Update index.ts

--- HCBackend/src/index.ts
+++ HCBackend/src/index.ts
@@ -7,14 +7,15 @@ import chatRoutes from "./routes/chatRoutes";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ['https://heal-chat-h523rk9ma-yamb1254s-projects.vercel.app','https://heal-chat.vercel.app/','https://heal-chat-yamb1254s-projects.vercel.app/','http://localhost:3000'], // Add your frontend URL here
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-  allowedHeaders: ['Content-Type','Authorization']
-}));
+// app.use(cors({
+//   origin: ['https://heal-chat-h523rk9ma-yamb1254s-projects.vercel.app','https://heal-chat.vercel.app/','https://heal-chat-yamb1254s-projects.vercel.app/','http://localhost:3000'], // Add your frontend URL here
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+//   allowedHeaders: ['Content-Type','Authorization']
+// }));
 
-app.options('*', cors());
+// app.options('*', cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
